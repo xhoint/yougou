210deb86ea6c76c4c497e15600af6e5cd722505b
change <xjx903697869@163.com>
2020-02-28 14:59:19 +0800

    04-轮播图

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import request from './utils/request.js'
 //app.js
 App({
-
+  // 公共页面加载时所用方法
+  onLaunch: function() {
+    // 添加基准路径
+    request.defaults.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,24 @@ import request from '../../utils/request.js'
 //获取应用实例
 
 Page({
-
+  data: {
+    // 轮播图数据
+    banners: [],
+  },
+  // 当前页面加载时的所有的方法
+  onLoad() {
+    // 请求轮播图接口
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      // message是轮播图的数组
+      const {
+        message
+      } = res.data;
+      // 赋值给banners
+      this.setData({
+        banners: message
+      })
+    })
+  }
 })
