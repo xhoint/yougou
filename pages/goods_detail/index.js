b8b20d5b8acd64f18e7c5bc868759a09293a89a8
change <xjx903697869@163.com>
2020-03-06 01:56:06 +0800

    13-商品详情页面

---
+++ pages/goods_detail/index.js
@@ -0,0 +1,77 @@
+// pages/goods_detail/index.js
+
+import request from "../../utils/request.js"
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // 商品的详情
+        detail: {},
+        // 记录tab当前的索引
+        current: 0,
+        // 需要做图片预览的数组
+        picUrls: []
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        // 请求商品详情
+        request({
+            url: "/goods/detail",
+            data: {
+                goods_id: options.id
+            }
+        }).then(res => {
+            const {
+                message
+            } = res.data;
+
+            // 获取图片的链接，给预览图片的接口使用
+            const picUrls = message.pics.map(v => {
+                return v.pics_big
+            });
+
+            // 保存数据到data
+            this.setData({
+                detail: message,
+                picUrls // 给预览图片的接口使用
+            })
+        })
+    },
+
+    // 商品详情的tab切换
+    handleTab(e) {
+        const {
+            index
+        } = e.currentTarget.dataset;
+
+        this.setData({
+            current: index
+        })
+    },
+
+    // 预览图片
+    handlePreview(e) {
+        // 获取当前点击的图片的索引值
+        const {
+            index
+        } = e.currentTarget.dataset;
+
+        wx.previewImage({
+            current: this.data.picUrls[index], // 当前显示图片的http链接
+            urls: this.data.picUrls // 需要预览的图片http链接列表
+        })
+    },
+
+    // 跳转到购物车页
+    handleToCart() {
+        wx.switchTab({
+            url: '/pages/cart/index',
+        })
+    }
+})
