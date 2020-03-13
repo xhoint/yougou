6647c1fa0d75c86011397446eabda978545cdf1a
change <xjx903697869@163.com>
2020-03-13 20:56:29 +0800

    21-个人中心页面

--- pages/info/index.js
+++ pages/info/index.js
@@ -11,21 +11,21 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
 
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
+    onReady: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
+    onShow: function() {
         if (typeof this.getTabBar === 'function' &&
             this.getTabBar()) {
             this.getTabBar().setData({
@@ -34,38 +34,52 @@ Page({
         }
     },
 
+    // 收货地址
+    handleAddress() {
+        // 根据接口的名字在文档里搜索
+        wx.chooseAddress();
+    },
+
+    // 联系客服
+    handleContact() {
+        // 根据接口的名字在文档里搜索
+        wx.makePhoneCall({
+            phoneNumber: '1340000' //仅为示例，并非真实的电话号码
+        })
+    },
+
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {
+    onHide: function() {
 
     },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {
+    onUnload: function() {
 
     },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
